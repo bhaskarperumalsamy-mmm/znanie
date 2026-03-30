a009fe0bc2c9e6831a7d2b29b42d1dc47867aa5f
bhaskarperumalsamy-mmm <bhaskarperumalsamy@gmail.com>
2026-03-30 17:14:33 +0530

    Delete debug_h1.js

--- debug_h1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const css = fs.readFileSync('c:\\bhaskar\\znaine_draft\\vistario.css', 'utf8');
-
-const query = 'h1';
-const regex = new RegExp(`(?:^|\\})[^{}]*${query}[^{}]*\\{([^}]*)\\}`, 'ig');
-let match;
-while ((match = regex.exec(css)) !== null) {
-  console.log(`Match: ${match[0]}`);
-}
