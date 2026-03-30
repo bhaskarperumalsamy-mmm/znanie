8b82302d26fb6ab217c0a975a949904c975fc774
bhaskarperumalsamy-mmm <bhaskarperumalsamy@gmail.com>
2026-03-30 17:14:45 +0530

    Delete debug_p.js

--- debug_p.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const css = fs.readFileSync('c:\\bhaskar\\znaine_draft\\vistario.css', 'utf8');
-
-const regex = /(?:^|\\})[^{}]*p[^{}]*\\{([^}]*)\\}/ig;
-let match;
-while ((match = regex.exec(css)) !== null) {
-  console.log(`Match: ${match[0]}`);
-}
